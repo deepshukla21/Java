81792c11fcc8a2b34c8046f0dc26ca78a058e485
deepshukla21 <deepshukla292@gmail.com>
2019-09-05 09:36:20 +0530

    Optimized code

--- algorithm/Binary.java
+++ algorithm/Binary.java
@@ -15,24 +15,38 @@ import com.bridgelabz.algorithm.BinaryConversion;
 public class Binary {
 	// function to swap the nibbles after providing proper padding to make it 8 bit
    public static String swapNibbles(String binary) {
-		 int padding = 8 - binary.length();
+	   Scanner sc = new Scanner(System.in);
+	   System.out.println("Enter the value for no of bits in which you want your result to come :");
+	   int required_bits = sc.nextInt();
+		 int padding = required_bits - binary.length();
 		 for(int i = 0; i < padding; i++) {
 			binary = "0" + binary;
+		}
+		// Close open resources
+		try {
+			if(sc != null) {
+				sc.close();
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
 		}
 	// swapping the nibbles	 
 	 binary = binary.substring(binary.length()/2) + binary.substring(0,binary.length() / 2);
 	 return binary;
    }
    // Function to convert binary string into decimal
-   public static void toDecimal(String bin) {
-	   double decimal = 0;
-	   for(int i = 0; i < bin.length(); i++) {
-		   if(bin.charAt(i) == '1') {
-			   decimal = decimal + Math.pow(2, bin.length() - 1 - i);
+   public static void toDecimal(String binary) {
+	   int number = Integer.parseInt(binary);
+	   int base = 1;
+	   int decimal = 0;
+	   while(number != 0) {
+		   if(number % 10 != 0) {
+			   decimal = decimal + (number % 10) * base;
 		   }
-	   }
-	      
-	   System.out.println("Decimal value is " + Math.round(decimal));
+		   base = base * 2;
+		   number = number / 10;
+	   }      
+	   System.out.println("Decimal value is " + decimal);
    }
    /* The main function is written to test Binary class
     */
