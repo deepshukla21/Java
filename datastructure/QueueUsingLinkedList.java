9e95f5f3192f3d777bc790a79b2cfa3e4a0b7763
Deep Shukla <deepshukla292@gmail.com>
2019-09-11 08:29:44 +0530

    updated code

--- datastructure/QueueUsingLinkedList.java
+++ datastructure/QueueUsingLinkedList.java
@@ -1,10 +1,5 @@
-/******************************************************************************
- *  Purpose: Created Queue using LinkedList and all it's method to perform required operations
- *  @author  Deep Shukla
- *  @version 1.0
- *  @date    07-08-2019
- ******************************************************************************/
 package com.bridgelabz.datastructure;
+
 
 class NodeQ<T>{
 	T data;
@@ -14,7 +9,7 @@ public class QueueUsingLinkedList<T> {
     NodeQ<T> first, last;
    
    // Method to add element in queue
-   public void enqueue(T data) {
+   public void enQueue(T data) {
 	   NodeQ<T> node = new NodeQ<T>();
 	   node.data = data;
 	   if(first == null) {
@@ -38,37 +33,20 @@ public class QueueUsingLinkedList<T> {
 	   first = first.next;
 	   return current.data; 
    }
-
-   //Method to get element at particular index.
-   public T readAt(int index) {
-       NodeQ<T> node = first;
-       for(int i=0;i<index;i++){
-           if(node != null) {
-               node = node.next;
-           } else {
-               return null;
-           }
-       }
-
-       return node.data;
-   }
-
    // Method to check number of elements in queue
    public int size() {
-	   int size = 0;
-	   if(first == null) {
-		   return size;
-	   }
-	   while(first.next != null) {
-		   size ++;
-		   first = first.next;
-	   }
-	   return size;
+	   int count = 0;
+		NodeQ<T> current = first;
+		while(current != null) {
+			count++;
+			current = current.next;
+		}
+		return count;
    }
    // Method to display element present in queue
    public void display() {
 	   if(first == null) {
-		   System.out.println("Stack is empty");
+		   System.out.println("Queue is empty");
 		   return;
 	   }
 	   NodeQ<T> current = first;
@@ -80,20 +58,6 @@ public class QueueUsingLinkedList<T> {
 	   }
 	   System.out.println();
    }
-   public void displayDeque() {
-	   if(first == null) {
-		   System.out.println("Queue is empty");
-		   return;
-	   }
-	   NodeQ<T> current = first;
-	   System.out.print(first.data + " ");
-	   current = first.previous;
-	   while(current != null) {
-		  System.out.print(current.data + " ");
-		  current = current.previous;
-	   }
-	   System.out.println();
-   }
    // Method to get first element in queue
    public T peek() {
 	   if(first == null) {
@@ -101,45 +65,50 @@ public class QueueUsingLinkedList<T> {
 	   }
 	   return first.data;
    }
-  // Method to add element at front
+   
    public void addFront(T data) {
 	   NodeQ<T> node = new NodeQ<T>();
+	   NodeQ<T> current;
 	   node.data = data;
 	   if(first == null) {
-        first  =   node;
+        first  = last =   node;
 	   }
 	   else {
-		   first.next = node;
-		   node.previous = first;
+		   current = first;
+		   current.previous = node;
 		   first = node;
+		   first.next = current;
 	   }
-	  // first = node;	  
+	 	  
    }
-   // Method to remove element from front
-   public T removeFront() {
+   public T removeLast() {
 	   if(first == null) {
-		   throw new NullPointerException("Dequeue is empty");
+		   throw new NullPointerException("Queue is empty");
 	   }
-	   NodeQ<T> current = first;
-	   first = first.previous;
-	   return current.data;
-   }
+	   NodeQ<T> current = first; 
+	   NodeQ<T>  previous =  null;
+		while(current.next != null) {
+			previous = current;
+			current = current.next;
+	 }
+		T data = current.data;
+		current = previous;
+		current.next = null;
+		 return data;
+		
+  }
  // Main method to test QueueUsingLinkedList class
    public static void main(String[] args) {
-	QueueUsingLinkedList<Integer> queue = new QueueUsingLinkedList<Integer>();
 	QueueUsingLinkedList<Integer> queue1 = new QueueUsingLinkedList<Integer>();
-	queue.enqueue(10);
-	queue.enqueue(20);
-	queue.enqueue(30);
-	queue.enqueue(40);
-	queue.display();
-	System.out.println(queue.deQueue());
-	System.out.println(queue.peek());
-	System.out.println("===Dequeue Operations======");
 	queue1.addFront(10);
 	queue1.addFront(20);
-	queue1.addFront(90);
-	queue1.displayDeque();
-}
-   
+	queue1.enQueue(30);
+	//queue1.enQueue(20);
+	queue1.addFront(10);
+	queue1.display();
+	queue1.removeLast();
+	queue1.display();
+	
+	
+	}
 }
